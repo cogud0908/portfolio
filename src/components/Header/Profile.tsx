2909b2d128fa33c23746f80e915dc914c1452fbc
임채형 <cogud0908@douzone.com>
2023-01-11 22:21:15 +0900

    📝 프로필 수정

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -9,7 +9,7 @@ const Header = () => {
             <div className="col-12 col-md-auto text-center text-md-start">
               <img
                 className="profile-image mb-3 mb-lg-0 ms-md-0 rounded mx-auto"
-                src="assets/images/profile.png"
+                src="/assets/images/profile.png"
                 alt=""
               />
             </div>
@@ -20,6 +20,7 @@ const Header = () => {
                 웹 프론트엔드 3년차 개발자입니다. 주로 공통 컴포넌트 개발을 진행했었으며, 범용성과 확장성에 UX/UI를
                 계획하며 개발하고 있습니다. 최근에는 서비스 개발에 참여하게 되어, 유용한 기능들을 모듈로 제공하는 것을
                 계획하고 있습니다. <br />
+                <br />
                 경험이 부족하지만 부분적으로 경험을 쌓아가며, 노력하고 있습니다.
               </div>
             </div>
